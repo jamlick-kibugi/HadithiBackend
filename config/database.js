49f4d6e605fa22461210603c8a6e0ab2d488e54b
jamlick-kibugi <67996337+jamlick-kibugi@users.noreply.github.com>
2025-03-25 19:29:10 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,18 @@
-require('dotenv').config() 
-module.exports={ 
-  "development": { 
-    "username": process.env.USERNAME, 
-    "password":  process.env.PASSWORD, 
-    "database":  process.env.DATABASE, 
-    "host":  process.env.HOST, 
-    "dialect": process.env.DIALECT 
-  } 
-}
+require('dotenv').config(); // Load environment variables from .env file
+
+module.exports = {
+  development: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+  },
+  production: {
+    username: process.env.PROD_USERNAME,
+    password: process.env.PROD_PASSWORD,
+    database: process.env.PROD_DATABASE,
+    host: process.env.PROD_HOST,
+    dialect: process.env.DIALECT,
+  },
+};
