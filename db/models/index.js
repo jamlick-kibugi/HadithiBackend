b457a64e709ba96de5c71883bf8f61b1dc13284e
jamlick-kibugi <67996337+jamlick-kibugi@users.noreply.github.com>
2025-03-25 19:16:57 +0300

    Update index.js

--- db/models/index.js
+++ db/models/index.js
@@ -25,11 +25,10 @@ const config = require(__dirname + '/../../config/database.js')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: config.dialect,
+});
 
 // fs
 //   .readdirSync(__dirname)
