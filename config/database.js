7dbdce76fddee9f303d23ab9e0192d752dbff087
jamlick-kibugi <67996337+jamlick-kibugi@users.noreply.github.com>
2025-03-25 19:30:30 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -9,10 +9,10 @@ module.exports = {
     dialect: process.env.DIALECT,
   },
   production: {
-    username: process.env.PROD_USERNAME,
-    password: process.env.PROD_PASSWORD,
-    database: process.env.PROD_DATABASE,
-    host: process.env.PROD_HOST,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
     dialect: process.env.DIALECT,
   },
 };
