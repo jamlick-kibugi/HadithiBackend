8fa020e83d249f7d198ce4cdde375a61422af5f0
jamlick-kibugi <67996337+jamlick-kibugi@users.noreply.github.com>
2025-03-28 00:08:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ require('dotenv').config();
 const port = 3000
 app.use(cors());
 app.use(express.json()); 
+
+
+// CORS configuration
+const allowedOrigins = ['http://localhost:3000', 'https://servicefiti.co.ke' , 'https://hadithi.co.ke'];
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+
+
+
 const collabRouter =  require('./routers/collabRouter')
 const storyRouter =  require('./routers/storyRouter')
 const authRouter = require('./routers/authRouter')
